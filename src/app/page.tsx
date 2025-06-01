901e5020d2e980cd259cbe6155ecf6db74eb1c38
S5HX <yarulinainur@gmail.com>
2025-06-02 00:29:36 +0700

    добавил временные метаданные

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+import type { Metadata } from "next"
 import { MainCarousel } from "@/widgets/main-carousel"
 import { Link } from "@heroui/link"
 import { collections, MediaCarousel } from "@/entities/media"
+
+export const metadata: Metadata = {
+  title: "LORDFILM - смотрите фильмы и сериалы онлайн бесплатно в хорошем качестве",
+  description:
+    "Бесплатно смотрите тысячи фильмов, сериалов, мультфильмов и аниме: от культовых хитов до свежих новинок. Находите уникальные истории для любого настроения",
+}
 
 export default async function Home() {
   const mainCollections = await collections.filter((c) => !c.parentSlug)
