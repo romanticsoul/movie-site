1b93d1d3dc685e1112d581be85d01b01b68c24a6
Admin <user@K307-04.college.spk>
2025-06-05 17:07:05 +0700

    Добавлен адаптив главной карусели

--- src/widgets/main-carousel/ui/carousel-item.tsx
+++ src/widgets/main-carousel/ui/carousel-item.tsx
@@ -10,6 +10,7 @@ export function CarouselItem({ media }: CarouselItemProps) {
   return (
     <div className="relative min-w-0 flex-shrink-0 flex-grow-0 basis-full">
       <span className="absolute inset-0 z-20 bg-gradient-to-t from-gray-950 from-0% to-transparent to-100% backdrop-blur-xl"></span>
+
       <Image
         priority
         fill
@@ -19,8 +20,9 @@ export function CarouselItem({ media }: CarouselItemProps) {
         className="z-10 object-cover"
       />
 
-      <div className="container relative z-30 grid h-full grid-cols-5 items-center gap-4 border-x border-default-600/20 py-16">
-        <div className="col-span-3 text-white">
+      <div className="container relative z-30 grid h-full items-center gap-4 border-x border-default-600/20 py-8 md:grid-cols-4 md:py-16 lg:grid-cols-5">
+        {/*  */}
+        <div className="text-white md:col-span-3">
           <h1 className="mb-2 text-4xl font-black">{media.title}</h1>
           <div className="mb-4 flex gap-2">
             <MediaAgeChip age={media.rating.age} />
@@ -34,9 +36,16 @@ export function CarouselItem({ media }: CarouselItemProps) {
             Смотреть сейчас
           </Button>
         </div>
-        <span></span>
-        <div className="col-span-1">
-          <MediaPoster src={media.images.poster} alt={media.title} />
+        {/*  */}
+        <span className="max-lg:hidden"></span>
+
+        {/*  */}
+        <div className="max-md:order-first">
+          <MediaPoster
+            src={media.images.poster}
+            alt={media.title}
+            className="mx-auto w-3/5 sm:w-2/6 md:w-full"
+          />
         </div>
       </div>
     </div>
