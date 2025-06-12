e61339406506bc8bf5c89df399ec17a04277d1ad
S5HX <yarulinainur@gmail.com>
2025-06-13 02:03:19 +0700

    102616219

--- src/shared/lib/yandexMetrika.tsx
+++ src/shared/lib/yandexMetrika.tsx
@@ -3,17 +3,20 @@ export function YandexMetrika() {
   return (
     <Script id="yandex-metrika" strategy="afterInteractive">
       {`
-        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      m[i].l=1*new Date();
+      for (var j = 0; j < document.scripts.length; j++) {
+        if (document.scripts[j].src === r) { return; }
+      }
+      k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,
+      a.parentNode.insertBefore(k,a)
+      })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-        ym(102616219, "init", {
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true
-        });
+      ym(102616219, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+      });
     `}
     </Script>
   )
